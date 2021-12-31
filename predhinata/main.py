3c9021ba4d8642a181f8ae410b900b55905538cd
KAWAI Shun <shun@osstech.co.jp>
2021-12-31 19:21:54 +0900

    poetryに対応
    各種開発ツールの導入
    * lint -> flake8
    * formatter -> autopep8, autoflake, isort
    * CI -> pre-commit

--- predhinata/main.py
+++ predhinata/main.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, render_template, request, url_for
-import numpy as np
-import cv2
-import pandas as pd
-import face_recognition
-import os
-import time
-import pickle
 import csv
+import os
+import pickle
+import time
+
+import cv2
+import numpy as np
+from face_recognition import face_encodings, face_locations, load_image_file
+from flask import Flask, render_template, request
 
 SAVE_DIR = "./tmp"
 if not os.path.isdir(SAVE_DIR):
@@ -30,26 +30,28 @@ ja_name_list = ja_name_list[1:]
 def index():
     return render_template('./index.html')
 
+
 @app.route('/good')
 def good():
     return 'aaa'
 
+
 @app.route('/result', methods=["get", "post"])
 def result():
     img_path_list = []
-    i = 0
     for file in request.files.getlist("avatar"):
         img = file.stream
         img_array = np.asarray(bytearray(img.read()), dtype=np.uint8)
         img = cv2.imdecode(img_array, 1)
-        face = face_recognition.face_locations(img)
+        face = face_locations(img)
         for top, right, bottom, left in face:
             t = round(time.time()*10000)
             save_path = f'{SAVE_DIR}/{t}.jpg'
-            cv2.imwrite(save_path, img[top:bottom,left:right])
+            cv2.imwrite(save_path, img[top:bottom, left:right])
             img_path_list.append(t)
 
     return render_template('./choose_img.html', img_path_list=img_path_list)
+
 
 def get_ranking(similar_list, n=3):
     '''
@@ -66,25 +68,30 @@ def get_ranking(similar_list, n=3):
         per_list.append(per)
     return m_list, per_list, n
 
+
 @app.route('/pred', methods=["get", "post"])
 def pred():
     path_list = request.form.getlist('img')
     for i in range(len(path_list)):
-        img = face_recognition.load_image_file(f'{SAVE_DIR}/{path_list[i]}.jpg')
-        cnv = face_recognition.face_encodings(img,\
-            known_face_locations=[(0,img.shape[1],img.shape[0],0)])
+        img = load_image_file(f'{SAVE_DIR}/{path_list[i]}.jpg')
+        known_face_locations = [(0, img.shape[1], img.shape[0], 0)]
+        cnv = face_encodings(img, known_face_locations=known_face_locations)
         if i == 0:
             cnv_list = cnv[0]
         else:
             cnv_list = np.concatenate([cnv_list, cnv[0]])
-    cnv_list = cnv_list.reshape((len(path_list),128))
+    cnv_list = cnv_list.reshape((len(path_list), 128))
 
     model = pickle.load(open('./model/SVC_hinata_model.sav', 'rb'))
-    #print(model.predict(cnv_list))
+    # print(model.predict(cnv_list))
     result = model.predict_proba(cnv_list)
     m_list, per_list, n = get_ranking(result)
-    #print(m_list, per_list)
-    return render_template('./result.html', m_list=m_list, per_list=per_list, n=n)
+    # print(m_list, per_list)
+    context = {'m_list': m_list,
+               'per_list': per_list,
+               'n': n}
+    return render_template('./result.html', **context)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
